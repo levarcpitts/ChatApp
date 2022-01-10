803f2edf5f20747270bdfc5f6c17eb4308c18d6f
kidvalor <87155806+kidvalor@users.noreply.github.com>
2022-01-10 12:19:29 -0500

    Add files via upload

---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,91 @@
+import React from 'react'; 
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview} from './';
+import SalesLoftIcon from '../assets/SalesLoft.png'
+import LogoutIcon from '../assets/logout.png'
+
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src={SalesLoftIcon} alt="SalesLoft" width="30" />
+            </div>
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner" onClick={logout}>
+                <img src={LogoutIcon} alt="Logout" width="30" />
+            </div>
+        </div>
+    </div>
+);
+
+const CompanyHeader = () => (
+    <div className="channel-list__header">
+        <p className="channel-list__header__text">Chat App</p>
+    </div>
+);
+
+const ChannelListContainer = () => {
+
+    const logout = () => {
+     cookies.remove('token');
+     cookies.remove('userId');
+     cookies.remove('username');
+     cookies.remove('fullName');
+     cookies.remove('avatarURL');
+     cookies.remove('hashedPassword');
+     cookies.remove('phoneNumber');
+
+     window.location.reload();
+     
+    }
+
+
+    return (
+        <>
+           <SideBar logout={logout} />
+           <div className="channel-list__list__wrapper">
+            <CompanyHeader />
+            <ChannelSearch />
+            <ChannelList 
+                filters={{}}
+                channelRenderFilterFn={() => {}}
+                List={(listProps) => (
+                    <TeamChannelList 
+                    {...listProps}
+                    type='team'
+                    />
+                )}
+                Preview={(previewProps) => (
+                    <TeamChannelPreview 
+                    {...previewProps}
+                    type = 'team'
+                    />
+                )}
+            />
+            <ChannelList 
+    filters={{}}
+    channelRenderFilterFn={() => {}}
+    List={(listProps) => (
+        <TeamChannelList 
+        {...listProps}
+        type='messaging'
+        />
+    )}
+    Preview={(previewProps) => (
+        <TeamChannelPreview 
+        {...previewProps}
+        type = 'messaging'
+        />
+    )}
+/>
+           </div>
+        </>
+    )
+}
+
+export default ChannelListContainer;
